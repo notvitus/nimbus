bd0c7c847ea622d6f056ea850daea49b53c2f110
baer1 <videocreator@outlook.de>
2024-09-20 16:27:18 +0200

    Upgrade to PyQt6

--- nimbus.py
+++ nimbus.py
@@ -1,6 +1,7 @@
-from PyQt5.QtCore import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtWebEngineWidgets import QWebEngineView
+from PyQt6.QtCore import *
+from PyQt6.QtGui import QAction
+from PyQt6.QtWidgets import *
+from PyQt6.QtWebEngineWidgets import QWebEngineView
 import sys
 
 
@@ -73,4 +74,4 @@ class MainWindow(QMainWindow):
 app = QApplication(sys.argv)
 app.setApplicationName("NIMBUS Browser")
 window = MainWindow()
-app.exec_()
+app.exec()
